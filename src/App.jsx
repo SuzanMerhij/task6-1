cb0a51691f9b2b33672780fb89b995c35ba36898
SuzanMerhij <suzanmerhij55@gmail.com>
2023-11-27 09:17:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Calculator from './components/Calculator/Calculator';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator/>
+
+    </div>
+  );
+}
+export default App;
